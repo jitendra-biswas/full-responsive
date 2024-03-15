e0fcf6fe9864d8df30c0e707051edaf086a12f81
jitendra-biswas <jitendrabiswas12344@gmail.com>
2024-03-15 18:03:20 +0545

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+// var area = document.querySelector("body")
+// var cursor = document.querySelector(".cursor")
+// area.addEventListener("mousemove", function(dets){
+//     cursor.style.left = dets.x+"px"
+//     cursor.style.top = dets.y+"px"
+// })
+
+var side = document.querySelector(".side-nav")
 var bar = document.querySelector("#bar")
-var elem = document.querySelectorAll(".content")
-bar.addEventListener("click", function(){
-    elem.style.display = "block"
+var value = 0
+bar.addEventListener("click",function(){
+    if(value == 0){
+        side.style.display = "block"
+        value = 1
+    }
+    else{
+        side.style.display = "none"
+        value = 0
+    }
 })
